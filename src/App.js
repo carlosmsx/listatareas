065f55fd3c6b8ca8519405b26847f2358dfe038a
carlosmsx <carlosmsx@gmail.com>
2022-07-21 11:31:41 -0300

    agregados los componentes Titulo y Subtitulo con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Subtitulo from './components/Subtitulo';
 import Titulo from './components/Titulo';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
     return (
         <div>
+            <section className="container">
             <Titulo/>
-            
+            <Subtitulo/>
+            </section>
         </div>
     );
 };
---
+++ src/components/Subtitulo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Subtitulo extends Component {
+    render() {
+        return (
+                <p className="text-center">Lista de tareas usando react y localstorage</p>
+        );
+    }
+}
+
+export default Subtitulo;
